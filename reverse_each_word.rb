96e6d13772d69fd1d350a0b53ce4d2982f2849f5
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 22:14:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   new_sentence = ''
   words.each do |word|
-    new_sentence << "#{word.reverse} "
+    new_sentence << "#{word.reverse}"
   end
   new_sentence
 
