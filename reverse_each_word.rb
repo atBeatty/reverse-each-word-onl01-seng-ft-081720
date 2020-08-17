a6c760c97064f52a645cfc747fe75b116be5932e
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 22:36:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,10 +6,12 @@ def reverse_each_word(sentence)
   end
 return result_array.join(" ")
 end
-# def reverse_each_word(sentence)
-#   words = sentence.split(" ")
-#   words.collect do |word|
-#     word.reverse
-#   end
-#   words.join(" ")
-# end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
