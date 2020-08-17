64ff1dcef5a602997482295c6b3a4c37028503d6
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 22:27:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,10 @@ def reverse_each_word(sentence)
 
 
 end
-def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  words.collect do |word|
-    word.reverse
-  end
-
-end
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ")
+#   words.collect do |word|
+#     word.reverse
+#   end
+#   words.join(" ")
+# end
